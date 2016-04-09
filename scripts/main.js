05fa27ff56387280077085813953fe75f8f167cf
Robert Tassy <Rtass001@fiu.edu>
2016-04-09 12:57:09 -0400

    required reactdom, added html into component

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,14 @@ This will make <StorePicker> element where I can put where I want.
 var StorePicker = React.createClass({
   render : function() {
     return (
-    <p>Hi</p>
+    <form className="store-selector">
+      <h2>Please enter a store</h2>
+      <input type="text" ref="storeId" required></input>
+      <input type="submit"></input>
+
+    </form>
     )
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
