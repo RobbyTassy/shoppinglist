2979fa69bd7eb985744ecdbe75df8aa8c8c990b1
Robert Tassy <Rtass001@fiu.edu>
2016-04-09 23:50:29 -0400

    base components

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// App
+
+var App = React.createClass ({
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header></Header>
+        </div>
+        <Order></Order>
+        <Inventory></Inventory>
+      </div>
+    )
+  }
+})
+
+// Header
+
+var Header = React.createClass ({
+  render : function() {
+    return (
+      <p>Header</p>
+    )
+  }
+})
+
+// Order
+
+var Order = React.createClass ({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+})
+
+// Inventory
+
+var Inventory = React.createClass ({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
+
 /*
 StorePicker
 This will make <StorePicker> element where I can put where I want.
@@ -19,4 +66,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
+ReactDOM.render(<App/>, document.querySelector('#main'))
